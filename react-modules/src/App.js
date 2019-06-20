e929db2fdbc6c4e8c888c99cb3fec052aaa8b289
Airen <weihua.lwh@alibaba-inc.com>
2019-06-21 00:13:11 +0800

    demo6: CSS Modules中属性选择器、标签选择器、伪类选择器的使用

--- react-modules/src/App.js
+++ react-modules/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from './Button/Button';
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <Button />
       </header>
     </div>
   );
