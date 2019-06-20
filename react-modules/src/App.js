488963dd7865056ee3a8751471a9eb82d5bc98d2
Airen <weihua.lwh@alibaba-inc.com>
2019-06-20 22:08:16 +0800

    demo2，CSS Modules的多类名的使用

--- react-modules/src/App.js
+++ react-modules/src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+
+import Button from './Button/Button';
+import cn from 'classnames';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <img src={logo} className={styles.AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <Button />
+
+        <div className={styles.composeClass}>合并别的文件中的类</div>
+
+        <div className={cn(styles.border, styles.box)}>classnames模块</div>
       </header>
     </div>
   );
---
+++ react-modules/src/Button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './Button.module.css';
+
+console.log(styles);
+
+class Button extends React.Component {
+    render() {
+        return (
+            <div className={styles.buttonPrimary} role="button">Button</div>
+        )
+    }
+}
+
+export default Button;
