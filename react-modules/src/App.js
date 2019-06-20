f753253a5a644ec8d2a00801e933658656066d8e
Airen <weihua.lwh@alibaba-inc.com>
2019-06-20 22:44:00 +0800

    demo3: CSS Modules组合选择器的使用

--- react-modules/src/App.js
+++ react-modules/src/App.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+
+console.log(styles);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <img src={logo} className={styles.AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+
+        <button className={styles.button}>Regular Button</button>
+        
+        <div className={styles.fun}>
+          <button className={styles.button}>FUN BUTTON</button>
+        </div>
+
+        <div className={styles.box}>
+          <button className={styles.button}>BOX BUTTON</button>
+        </div>
+
+        <button className={`${styles.button} ${styles.disabled}`}>Disabled Button</button>
       </header>
     </div>
   );
