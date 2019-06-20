9ecf990fc2682cdd0a61cac8099eeb31f005189d
Airen <weihua.lwh@alibaba-inc.com>
2019-06-20 20:26:26 +0800

    demo1, CSS Modules中类名的使用

--- react-modules/src/App.js
+++ react-modules/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+
+import Button from './Button/Button';
+
+console.log(styles)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className={styles.App__header}>
+        <img src={logo} className={styles.App_logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +22,11 @@ function App() {
         >
           Learn React
         </a>
+
+        <Button />
+
+        <div className="button">我是一个带button类名的元素</div>
+
       </header>
     </div>
   );
---
+++ react-modules/src/Button/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+console.log(styles)
+
+class Button extends React.Component {
+    render() {
+        return (
+            <div className={styles.button} role="button">Click Me</div>
+        )
+    }
+}
+
+export default Button;
