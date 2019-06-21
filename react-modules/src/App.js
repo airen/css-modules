a544b30e511f5e0374d96209c61873a769df904c
Airen <weihua.lwh@alibaba-inc.com>
2019-06-21 17:08:04 +0800

    demo10: 使用 babel-plugin-react-css-modules实现CSS Modules

--- react-modules/src/App.js
+++ react-modules/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import appStyle from './App.css';
+import fooStyle from './foo.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div styleName="App">
+      <header styleName="App-header">
+        <img src={logo} styleName="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          styleName="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          className="active"
         >
           Learn React
         </a>
+        <div styleName="appStyle.active">active class in App.css</div>
+        <div styleName="fooStyle.active">active class in foo.css</div>
       </header>
     </div>
   );
